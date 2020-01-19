2668bf885440c609352b7a2ad3645293b35102c0
three4c <three4c@gmail.com>
2020-01-19 23:57:58 +0900

    pert3途中

--- src/part3/index.js
+++ src/part3/index.js
@@ -1,30 +1,80 @@
-const http = require('http');
+const express = require('express');
+const app = express();
+const bodyParser = require('body-parser');
 
-let notes = [
+let persons = [
   {
-    id: 1,
-    content: 'HTML is easy',
-    date: '2019-05-30T17:30:31.098Z',
-    important: true
+    name: 'Arto Hellas',
+    number: '040-123456',
+    id: 1
   },
   {
-    id: 2,
-    content: 'Browser can execute only Javascript',
-    date: '2019-05-30T18:39:34.091Z',
-    important: false
+    name: 'Ada Lovelace',
+    number: '39-44-5323523',
+    id: 2
   },
   {
-    id: 3,
-    content: 'GET and POST are the most important methods of HTTP protocol',
-    date: '2019-05-30T19:20:14.298Z',
-    important: true
+    name: 'Dan Abramov',
+    number: '12-43-234345',
+    id: 3
+  },
+  {
+    name: 'Mary Poppendieck',
+    number: '39-23-6423122',
+    id: 4
   }
 ];
-const app = http.createServer((request, response) => {
-  response.writeHead(200, { 'Content-Type': 'application/json' });
-  response.end(JSON.stringify(notes));
+
+const generateId = () => {
+  const maxId = persons.length > 0 ? Math.max(...persons.map(n => n.id)) : 0;
+  return maxId + 1;
+};
+
+app.use(bodyParser.json());
+
+app.get('/info', (request, response) => {
+  response.send(`
+  <p>Phonebook has info for ${persons.length} people</p>
+  <p>${new Date()}</p>
+  `);
 });
 
-const port = 3001;
-app.listen(port);
-console.log(`Server running on port ${port}`);
+app.get('/persons', (request, response) => {
+  response.json(persons);
+});
+
+app.get('/persons/:id', (request, response) => {
+  const id = Number(request.params.id);
+  const person = persons.find(person => person.id === id);
+  person ? response.json(person) : response.status(404).end();
+});
+
+app.delete('/persons/:id', (request, response) => {
+  const id = Number(request.params.id);
+  persons = persons.filter(person => person.id !== id);
+
+  response.status(204).end();
+});
+
+app.post('/persons', (request, response) => {
+  const body = request.body;
+
+  if (!body) {
+    return response.status(400).json({
+      error: 'content missing'
+    });
+  }
+
+  const person = {
+    name: body.name,
+    number: body.number,
+    id: generateId()
+  };
+
+  persons = persons.concat(person);
+});
+
+const PORT = 3001;
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
