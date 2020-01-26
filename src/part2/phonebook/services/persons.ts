143917b7f61c71ecd2b9463e0d14f4f2d5a0743e
three4c <three4c@gmail.com>
2020-01-26 10:57:51 +0900

    修正

--- src/part2/phonebook/services/persons.ts
+++ src/part2/phonebook/services/persons.ts
@@ -1,5 +1,5 @@
 import axios from 'axios';
-const baseUrl = 'http://localhost:3001/persons';
+const baseUrl = 'https://full-stack-open2019.herokuapp.com/persons';
 
 interface PersonProps {
   items: {
