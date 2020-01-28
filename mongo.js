a8ed3e1b4fd88fb90d8d293020b99737efc94839
three4c <three4c@gmail.com>
2020-01-28 23:58:09 +0900

    サンプル動かない

--- mongo.js
+++ mongo.js
@@ -1,10 +1,42 @@
-const MongoClient = require('mongodb').MongoClient;
-const uri =
-  'mongodb+srv://fullstackopen2019:abokado0514@cluster0-ptyni.mongodb.net/test?retryWrites=true&w=majority';
-const client = new MongoClient(uri, { useNewUrlParser: true });
-client.connect(err => {
-  const collection = client.db('test').collection('devices');
-  // perform actions on the collection object
-  collection.insertMany([{ name: '花子' }, { name: '一郎' }]);
-  client.close();
+// const MongoClient = require('mongodb').MongoClient;
+// const uri =
+//   'mongodb+srv://fullstackopen2019:abokado0514@cluster0-ptyni.mongodb.net/phonenumber-app?retryWrites=true&w=majority';
+// const client = new MongoClient(uri, { useNewUrlParser: true });
+// client.connect(err => {
+//   const collection = client.db('phonenumber-app').collection('persons');
+//   // perform actions on the collection object
+//   collection.insertMany([{ name: '花子' }, { name: '一郎' }]);
+//   client.close();
+// });
+
+const mongoose = require('mongoose');
+
+if (process.argv.length < 3) {
+  console.log('give password as argument');
+  process.exit(1);
+}
+
+const password = process.argv[2];
+
+const url = `mongodb+srv://fullstackopen2019:${password}@cluster0-ptyni.mongodb.net/phonenumber-app?retryWrites=true&w=majority`;
+
+mongoose.connect(url, { useNewUrlParser: true });
+
+const noteSchema = new mongoose.Schema({
+  content: String,
+  date: Date,
+  important: Boolean
+});
+
+const Note = mongoose.model('Note', noteSchema);
+
+const note = new Note({
+  content: 'HTML is Easy',
+  date: new Date(),
+  important: true
+});
+
+note.save().then(response => {
+  console.log('note saved!');
+  mongoose.connection.close();
 });
