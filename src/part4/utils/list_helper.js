5da3f195e1ff32fcae3a71c991c87cf6ad4266d5
three4c <three4c@gmail.com>
2020-02-16 01:07:25 +0900

    test

--- src/part4/utils/list_helper.js
+++ src/part4/utils/list_helper.js
@@ -1,12 +1,11 @@
-const dummy = blogs => {
-  return 1;
-};
-
 const totalLikes = blogs => {
-  return blogs.length;
+  const reducer = (sum, item) => {
+    return sum + item.likes;
+  };
+
+  return blogs.reduce(reducer, 0);
 };
 
 module.exports = {
-  dummy,
   totalLikes
 };
