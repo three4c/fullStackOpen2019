e41b8c6482d4cbacd519339b3f6b076ad707615c
three4c <three4c@gmail.com>
2020-02-22 01:36:16 +0900

    テストを開始する前は初期化して空にする

--- src/part4/tests/blog_api.test.js
+++ src/part4/tests/blog_api.test.js
@@ -1,10 +1,24 @@
 const mongoose = require('mongoose');
 const supertest = require('supertest');
 const app = require('../app');
+const Blog = require('../models/blog');
 
 const api = supertest(app);
 
-test('notes are returned as json', async () => {
+const initialBlogs = [
+  {
+    title: 'hoge'
+  }
+];
+
+beforeEach(async () => {
+  await Blog.deleteMany({});
+
+  let blogObject = new Blog(initialBlogs[0]);
+  await blogObject.save();
+});
+
+test('blogs are returned as json', async () => {
   await api
     .get('/api/blogs')
     .expect(200)
@@ -14,13 +28,13 @@ test('notes are returned as json', async () => {
 test('there are five blogs', async () => {
   const response = await api.get('/api/blogs');
 
-  expect(response.body.length).toBe(4);
+  expect(response.body.length).toBe(initialBlogs.length);
 });
 
 test('the first blogs is about HTTP methods', async () => {
   const response = await api.get('/api/blogs');
-
-  expect(response.body[0].content).toBe('HTML is easy');
+  const title = response.body.map(r => r.title);
+  expect(title).toContain('hoge');
 });
 
 afterAll(() => {
