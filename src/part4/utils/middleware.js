6fe87e4557dcc40f06cd6a4b8f0ae5c5a50da8d0
three4c <three4c@gmail.com>
2020-05-07 00:43:14 +0900

    tokenだけでじゃpostエラーになり、userIdとidが必要なのが意味不明

--- src/part4/utils/middleware.js
+++ src/part4/utils/middleware.js
@@ -19,13 +19,16 @@ const errorHandler = (error, _, response, next) => {
     return response.status(400).send({ error: 'malformatted id' });
   } else if (error.name === 'ValidationError') {
     return response.status(400).json({ error: error.message });
+  } else if (error.name === 'JsonWebTokenError') {
+    return response.status(401).json({
+      error: 'invalid token',
+    });
   }
-
   next(error);
 };
 
 module.exports = {
   requestLogger,
   unknownEndpoint,
-  errorHandler
+  errorHandler,
 };
